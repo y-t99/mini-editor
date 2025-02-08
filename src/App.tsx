6a95c7e9b376aa9c8ee2d2125d217a5a03fc155e
itou ng <13060889537@sina.cn>
2025-02-08 14:55:47 +0800

    Enhance Lexical editor styling and layout with improved container and code styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
-import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
 import FloatingMenuPlugin from "./FloatingMenuPlugin";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 
 export default function App() {
   return (
-    <div className="p-[100px]">
-      <div className="bg-zinc-100 h-[100px] p-[5px] rounded-lg">
+    <div className="bg-zinc-100 p-[100px] h-screen">
+      <div className="border-2 border-zinc-950 bg-white min-h-[300px] p-[10px] rounded-lg">
         <div className="px-[5px] overflow-y-auto">
+
           <LexicalComposer initialConfig={
               { 
                 namespace: 'mini-editor', 
@@ -22,6 +22,7 @@ export default function App() {
                     italic: "italic",
                     strikethrough: "line-through",
                     underlineStrikethrough: "underlined-line-through",
+                    code: "bg-gray-100 dark:bg-gray-800 rounded-md p-2 text-sm",
                   },
                 } 
               }
